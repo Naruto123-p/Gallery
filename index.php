c492cc44fd0c82632da0e95208dc87d4371ccb9a
Kira <71603057+Naruto123-p@users.noreply.github.com>
2022-05-11 00:55:48 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Artsy</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <nav>
+        <label class="logo">Artsy</label>
+        <ul>
+            <li><a href="">Home</a><li>
+            <li><a href="">Contact</a><li>
+            <li><a href="">Arts</a><li>   
+            <li><a href="">Rules</a><li>     
+            <li><a href="" class="btn btn-success">login</a><li>     
+
+        </ul>
+
+    </nav>
+    <div class="section1">
+        <label class="img_text">The whole world is an art gallery when you're mindful.</label>
+        <img class="main_img" src="loginup.jpg">
+
+
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="welcome_img" src="front2.webp">
+
+            </div>
+            <div class="col-md-8">
+                <h1>Welcome To Artsy</h1>
+                <p>Welcome to online artstore Artsy.Here we provide the latest works of artist.You can upload your art and buy other arts made by other artists.We hope you enjoy your visit</p>    
+
+            </div>
+
+        </div>
+
+    </div>    
+
+    <center>
+        <h1>Our Arts Sections</h1>
+
+
+    </center>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="arts" src="digital.jpg">
+                <h1>Digital Arts</h1>
+
+            </div>
+            <div class="col-md-4">
+              <img class="arts" src="popart.jpg">
+              <h1>Pop Arts</h1>
+
+            </div>
+            <div class="col-md-4">
+            <img class="arts" src="oilpainting.jpg">
+            <h1>Oil Paintings</h1>
+
+            </div>
+            <div class="col-md-4">
+            <img class="arts" src="mozaic.jpg">
+            <h1>Mozaic Arts</h1>
+
+            </div>
+            <div class="col-md-4">
+            <img class="arts" src="handicraft.jpg">
+            <h1>Handicradts</h1>
+
+            </div>
+            <div class="col-md-4">
+            <img class="arts" src="other.jpg">
+            <h1>Others</h1>
+            
+
+            </div>
+            
+
+        </div>
+
+    </div>
+    <center>
+      <h1>Login Form</h1>
+    </center>
+    <div align="center" class="Login_form">
+        <form>
+            <div class="log_int">
+                <label class="label_text">Name</label>
+                <input class="input_deg" type="text" name="">
+            </div>
+            <div class="log_int">
+                <label class="label_text">Email</label>
+                <input class="input_deg" type="text" name="">
+            </div>
+            <div class="log_int">
+                <label class="label_text">Phone</label>
+                <input class="input_deg" type="text" name="">
+            </div class="log_int">
+            <div class="log_int">
+                <label class="label_text">Message</label>
+                <textarea class="input_txt"></textarea>
+            </div>
+            <div class="log_int">
+                
+                <input class="btn btn-primary" id="submit" type="submit" value="Apply" >
+            </div>
+
+
+        </form>
+
+    </div>
+
+    <footer>
+        <h3 class="footer_text">Artsy @copyright by Artsy authority</h3>
+    </footer>
+</body>
+</html>
+
